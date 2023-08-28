990f72dd5769fbf5240d7b29a77ea56c8628f6c7
miller1995 <nircaanton95@gmail.com>
2023-08-28 16:52:29 +0300

    - solved new problem -> isNar;

---
+++ src/Test.java
@@ -0,0 +1,28 @@
+
+public class Test {
+
+    public static void main(String[] args) {
+        String s = "9";
+        System.out.println(isNar(s));
+    }
+
+    public static boolean isNar(String str){
+        int number = Integer.parseInt(str);
+        System.out.println(number);
+        char [] c = str.toCharArray();
+
+        double result = 0;
+        for (int i  = 0; i < c.length; i++){
+            int ch = Integer.parseInt(String.valueOf(c[i]));
+
+            if( ch >= 0 && ch <= 9){
+                result += (Math.pow(ch, c.length));
+            } else
+                return false;
+        }
+            if (result == number)
+                return true;
+            else
+                return false;
+    }
+}
