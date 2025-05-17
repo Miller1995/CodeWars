ffcb09056034578e2d27e5f8b3732fec966dd56b
miller1995 <nircaanton95@gmail.com>
2025-05-17 13:15:22 +0300

    - solved new problem -> isNar;

--- src/NarcissisticNumber.java
+++ src/NarcissisticNumber.java
@@ -1,28 +1,29 @@
 
-public class Test {
+
+public class NarcissisticNumber {
 
     public static void main(String[] args) {
         String s = "9";
         System.out.println(isNar(s));
     }
 
-    public static boolean isNar(String str){
+    public static boolean isNar(String str) {
         int number = Integer.parseInt(str);
-        System.out.println(number);
-        char [] c = str.toCharArray();
-
+        char[] c = str.toCharArray();
         double result = 0;
-        for (int i  = 0; i < c.length; i++){
+
+        for (int i = 0; i < c.length; i++) {
             int ch = Integer.parseInt(String.valueOf(c[i]));
 
-            if( ch >= 0 && ch <= 9){
+            if (ch >= 0 && ch <= 9) {
                 result += (Math.pow(ch, c.length));
             } else
                 return false;
         }
-            if (result == number)
-                return true;
-            else
-                return false;
+
+        if (result == number)
+            return true;
+        else
+            return false;
     }
 }
